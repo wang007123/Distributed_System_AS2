43b6acb9433ce5ad09beb4803579a0abf17d910f
zhenghui wang <zhenghui.wang.k@gmail.com>
2022-08-03 21:20:40 +0800

    Add files via upload

---
+++ timeoutTest.java
@@ -0,0 +1,18 @@
+//Zhenghui Wang a1694827
+import java.net.*;
+import java.io.*;
+
+public class timeoutTest{
+   	public static void main(String [] args) {
+   		try{
+   			//send a normal PUT request and check the result in server side
+	      	Thread object = new Thread(new Content("localhost:4567","feed1.txt",0,""));
+    		object.start();
+   		}catch (Exception e) {
+			//throw error
+            System.err.println("thread exception:" + e.toString());
+		}
+
+   	}
+}
+
